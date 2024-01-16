f89e231a07a1e4a5c76d0dc04d294ed6651437bc
Amaurytus <amaury.samuel@epitech.digital>
2024-01-16 17:01:43 +0100

    login updated and register

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,7 +1,87 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Col, Row, Button, FormGroup, Input } from "reactstrap";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { Link, useNavigate } from "react-router-dom";
+import { storeUser } from "./helpers";
 
-function LoginPage() {
-  return <div>Login Page</div>;
-}
+const initialUser = { password: "", identifier: "" };
 
-export default LoginPage;
+const Login = () => {
+  const [user, setUser] = useState(initialUser);
+  const navigate = useNavigate();
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setUser((currentUser) => ({
+      ...currentUser,
+      [name]: value,
+    }));
+  };
+
+  const handleLogin = async () => {
+    const url = `http://localhost:1337/ api/auth/local`;
+    try {
+      if (user.identifier && user.password) {
+        const { data } = await axios.post(url, {
+          identifier: user.identifier,
+          password: user.password,
+        });
+        if (data.jwt) {
+          storeUser(data);
+          toast.success("Logged in successfully!", {
+            hideProgressBar: true,
+          });
+          setUser(initialUser);
+          navigate("/");
+        }
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        toast.error("Bad Request. Le serveur ne peut pas traiter votre demande en l'état actuel. Assurez-vous que les informations saisies sont correctes et essayez à nouveau.", {
+          hideProgressBar: true,
+        });
+      } else {
+        toast.error(error.message, {
+          hideProgressBar: true,
+        });
+      }
+    }
+  };
+
+  return (
+      <Row className="login">
+        <Col sm="12" md={{ size: 4, offset: 4 }}>
+          <div>
+            <h2>Login:</h2>
+            <FormGroup>
+              <Input
+                  type="email"
+                  name="identifier"
+                  value={user.identifier}
+                  onChange={handleChange}
+                  placeholder="Enter your email"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Input
+                  type="password"
+                  name="password"
+                  value={user.password}
+                  onChange={handleChange}
+                  placeholder="Enter password"
+              />
+            </FormGroup>
+            <Button color="primary" onClick={handleLogin}>
+              Login
+            </Button>
+            <h6>
+              Click <Link to="/registration">Here</Link> to sign up
+            </h6>
+          </div>
+        </Col>
+      </Row>
+  );
+};
+
+export default Login;
