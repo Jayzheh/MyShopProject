8d274f91e09b36fda31dcac8233f254bf705c582
Dan <danlyn1.bayan-medou@epitech.digital>
2024-01-17 17:33:45 +0100

    Products Table done

--- frontend/src/components/ProductsTable.js
+++ frontend/src/components/ProductsTable.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './Table.css';
+
+const jwtToken = process.env.REACT_APP_JWT_TOKEN;
 
 function ProductsTable() {
   const [products, setProducts] = useState([]);
@@ -56,11 +59,9 @@ function ProductsTable() {
   };
 
   const handleEdit = (productId, updatedProduct) => {
-    const jwtToken =
-      'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3MDU1MDQyMjgsImV4cCI6MTcwNTUwNzgyOCwicm9sZXMiOlsiUk9MRV9BRE1JTiIsIlJPTEVfVVNFUiJdLCJ1c2VybmFtZSI6ImRhbkBkYW4uZGFuIn0.hwfXrbK-HEsN03RvgbsGNxMbPNHdTX4mLKJsS4hlc9o3L4BJKjdKYsUhm4P3dP2WGms-4xmcdhUFlWiK7V8jpgZMVZMbqtkI7g8-Efu9hmezVoJO3L2ZK4K9svMf2uEtT3368mvLDlox4_id37tczN44Mhd587JB1AWPj0D9aNVStXUkDC3_6PLG97Vi6cPZrHC11dCyC-76XSdwcxrknASfKWz0FCiJ6xv9mabnZJ7XB3CwOlmqAABy-9QJLpQAIEd2zvlviZGisK3WtP_EzXxITiKvnpP0THWRvgXYGVLCLYEJMgZQrAMH9pMEwHuR8_8D5yP0JEkylElY0dIMEw'; // Replace withi your actual JWT token
+    //const jwtToken ='eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3MDU1MDQyMjgsImV4cCI6MTcwNTUwNzgyOCwicm9sZXMiOlsiUk9MRV9BRE1JTiIsIlJPTEVfVVNFUiJdLCJ1c2VybmFtZSI6ImRhbkBkYW4uZGFuIn0.hwfXrbK-HEsN03RvgbsGNxMbPNHdTX4mLKJsS4hlc9o3L4BJKjdKYsUhm4P3dP2WGms-4xmcdhUFlWiK7V8jpgZMVZMbqtkI7g8-Efu9hmezVoJO3L2ZK4K9svMf2uEtT3368mvLDlox4_id37tczN44Mhd587JB1AWPj0D9aNVStXUkDC3_6PLG97Vi6cPZrHC11dCyC-76XSdwcxrknASfKWz0FCiJ6xv9mabnZJ7XB3CwOlmqAABy-9QJLpQAIEd2zvlviZGisK3WtP_EzXxITiKvnpP0THWRvgXYGVLCLYEJMgZQrAMH9pMEwHuR8_8D5yP0JEkylElY0dIMEw'; 
 
-    axios
-      .put(`http://localhost/api/products/${productId}`, updatedProduct, {
+    axios.put(`http://localhost/api/products/${productId}`, updatedProduct, {
         headers: {
           Authorization: `Bearer ${jwtToken}`,
           'Content-Type': 'application/json',
@@ -85,7 +86,7 @@ function ProductsTable() {
     // Confirm before deleting
     if (window.confirm(`Are you sure you want to delete product with ID: ${productId}?`)) {
       // Retrieve the JWT token from environment variables or elsewhere
-      const jwtToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3MDU1MDQyMjgsImV4cCI6MTcwNTUwNzgyOCwicm9sZXMiOlsiUk9MRV9BRE1JTiIsIlJPTEVfVVNFUiJdLCJ1c2VybmFtZSI6ImRhbkBkYW4uZGFuIn0.hwfXrbK-HEsN03RvgbsGNxMbPNHdTX4mLKJsS4hlc9o3L4BJKjdKYsUhm4P3dP2WGms-4xmcdhUFlWiK7V8jpgZMVZMbqtkI7g8-Efu9hmezVoJO3L2ZK4K9svMf2uEtT3368mvLDlox4_id37tczN44Mhd587JB1AWPj0D9aNVStXUkDC3_6PLG97Vi6cPZrHC11dCyC-76XSdwcxrknASfKWz0FCiJ6xv9mabnZJ7XB3CwOlmqAABy-9QJLpQAIEd2zvlviZGisK3WtP_EzXxITiKvnpP0THWRvgXYGVLCLYEJMgZQrAMH9pMEwHuR8_8D5yP0JEkylElY0dIMEw'; // Replace with your actual JWT token
+      //const jwtToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3MDU1MDQyMjgsImV4cCI6MTcwNTUwNzgyOCwicm9sZXMiOlsiUk9MRV9BRE1JTiIsIlJPTEVfVVNFUiJdLCJ1c2VybmFtZSI6ImRhbkBkYW4uZGFuIn0.hwfXrbK-HEsN03RvgbsGNxMbPNHdTX4mLKJsS4hlc9o3L4BJKjdKYsUhm4P3dP2WGms-4xmcdhUFlWiK7V8jpgZMVZMbqtkI7g8-Efu9hmezVoJO3L2ZK4K9svMf2uEtT3368mvLDlox4_id37tczN44Mhd587JB1AWPj0D9aNVStXUkDC3_6PLG97Vi6cPZrHC11dCyC-76XSdwcxrknASfKWz0FCiJ6xv9mabnZJ7XB3CwOlmqAABy-9QJLpQAIEd2zvlviZGisK3WtP_EzXxITiKvnpP0THWRvgXYGVLCLYEJMgZQrAMH9pMEwHuR8_8D5yP0JEkylElY0dIMEw'; // Replace with your actual JWT token
 
       fetch(`http://localhost/api/products/${productId}`, {
         method: 'DELETE',
@@ -110,7 +111,7 @@ function ProductsTable() {
   return (
     <div>
       <h2>Products</h2>
-      <table>
+      <table className="products-table">
         <thead>
           <tr>
             <th>ID</th>
@@ -135,7 +136,7 @@ function ProductsTable() {
                   ))}
                 </td>
                 <td>
-                  <button onClick={() => startEdit(product)}>Edit</button>
+                  <button className="edit-button" onClick={() => startEdit(product)}>Edit</button>
                   <button onClick={() => handleDelete(product.id)}>Delete</button>
                 </td>
               </tr>
