b78432666c502c6526e6e8ab6a31a104deed5aa3
KYRIE KANE <146706868+meardagoat@users.noreply.github.com>
2024-01-18 15:34:18 +0100

    pre final push

--- frontend/src/components/RegisterPage.js
+++ frontend/src/components/RegisterPage.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './RegisterPage.css';
+
 
 function RegisterPage() {
   const [username, setUsername] = useState('');
